848a4b3d853cbf9923102c615bd63674d628928b
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2019-08-20 10:38:08 +0530

    Add files via upload

---
+++ chararry.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+push(char,char arr[20],char exp[20],int *top);
+int precedence(char arr[20],char exp[20],int *top);
+void pop(char arr[20],char exp[20],int *top);
+int main()
+{
+printf("Enter an infix expression\n");
+gets(arr);
+
